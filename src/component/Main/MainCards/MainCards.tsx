86d073f7092f71f6099450d6a7253bffcb8cf732
Kirill <93765186+Arsaide@users.noreply.github.com>
2023-11-20 22:41:59 +0200

    Added cards section. Home page completed

--- src/component/Main/MainCards/MainCards.tsx
+++ src/component/Main/MainCards/MainCards.tsx
@@ -4,7 +4,26 @@ import styles from './MainCards.module.scss';
 const MainCards = () => {
     return (
         <section className={styles.cards}>
-            cards
+            <div className={styles.cards__container}>
+                <article className={styles.card}>
+                    What <br/>We <br/> Do
+                </article>
+                <article className={styles.card}>
+                    Environment<br/> and Climate<br/> Action
+                </article>
+                <article className={styles.card}>
+                    Good<br/> Governance
+                </article>
+                <article className={styles.card}>
+                    Inclusive<br/> Economic<br/> Growth
+                </article>
+                <article className={styles.card}>
+                    Regional and<br/> International<br/> Relations
+                </article>
+                <article className={styles.card}>
+                    Women’s<br/> Empowerment and<br/> Gender Equality
+                </article>
+            </div>
         </section>
     );
 };
